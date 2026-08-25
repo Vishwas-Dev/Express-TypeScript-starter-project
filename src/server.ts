310d09217a256259f98c1e8a0d120805913fe9d9
Vishwas-Dev <vishwasgupta795@gmail.com>
2026-08-25 13:01:02 +0530

    Added api versioning

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import  express  from 'express';
-import {  serverConfig } from './config/index.js';
+import express from 'express';
+import { serverConfig } from './config/index.js';
+import v1Router from './routers/v1/index.router.js';
+import v2Router from './routers/v2/index.router.js';
+
 const app = express();
 
-// const PORT = 3000;
+/**
+ * Registering all the routers and their corresponding routes with out app server object.
+ */
+app.use('/api/v1', v1Router);
+app.use('/api/v2', v2Router);
 
-app.get('/ping', (req, res)=>{
 
-    res.send("pong");
-    console.log("pong");
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
+    console.log(`Press Ctrl+C to stop the server`);
 });
-
-console.log("enviromental variable loaded");
-
-app.listen(serverConfig.PORT, () =>{             
-    console.log(` server is running on this http//:localhost:${serverConfig.PORT}`);
-  
-    
-})
